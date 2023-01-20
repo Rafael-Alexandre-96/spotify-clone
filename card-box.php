ff971ccb9806b7f760e3438c8c0792064c87eb94
Rafael Alexandre <rafael.alexandre.1996.ra@gmail.com>
2023-01-19 21:32:34 -0300

    Subindo código simples sem PHP

--- card-box.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function showCardBox($name) { ?>
-    <div class="card-box grow-1">
-        <img class="img-card-box" src="img/uv-grid.jpg"/>
-        <p><?= $name ?></p>
-        <p class="gray-text">Artist</p>
-    </div>
-<?php } ?>
--- card-horizontal.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function showCardHorizontal($name) { ?>
-    <div class="card-horizontal grow-1 center-y">
-        <img class="img-card-horizontal" src="img/uv-grid.jpg"/>
-        <p><?= $name ?></p>
-        <i class="bi bi-play-fill"></i>
-    </div>
-<?php } ?>
